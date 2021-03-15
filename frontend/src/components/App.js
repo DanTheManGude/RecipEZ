afba28d5153e0844373a29b9aabdcd13e63eba25
mxk7405 <mxk7405@g.rit.edu>
2021-03-15 15:30:42 -0500

    wow

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,26 +3,35 @@
 import React from "react";
 import Routes from "./Routes.js";
 import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 function App() {
   return (
     <div id="app">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">RecipeZ</Typography>
+          <Link to="/signup">
+            <Button color="">Sign Up</Button>
+          </Link>
+          <Link to="/signin">
+            <Button color="">Sign In</Button>
+          </Link>
+          <Link to="/searchRecipe">
+            <Button color="">Recipe Search</Button>
+          </Link>
+          <Link to="/newRecipe">
+            <Button color="">New Recipe</Button>
+          </Link>
+          <Link to="/home">
+            <Button color="">Home</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <ul>
-        <Link to="/signup">
-          <li>Sign Up</li>
-        </Link>
-        <Link to="/signin">
-          <li>Sign In</li>
-        </Link>
-        <Link to="/searchRecipe">
-          <li>Recipe Search</li>
-        </Link>
-        <Link to="/newRecipe">
-          <li>NewRecipe</li>
-        </Link>
-        <Link to="/home">
-          <li>Home</li>
-        </Link>
         <Routes />
       </ul>
     </div>
